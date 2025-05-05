34e1ae5988ec6b363cc46b0f5aab7dc45d3f3172
kazamijimin <mendozanicknarry@gmail.com>
2025-05-05 23:29:52 +0800

    features

---
+++ src/app/features/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CoreFeatures from '@/components/CoreFeatures'
+function page() {
+  return (
+    <div>
+      <CoreFeatures/>
+    </div>
+  )
+}
+
+export default page
